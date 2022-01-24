63f936051a60d947a3b440bd37eee7e263bd2fbd
Brandon-Valentine <91150793+Brandon-Valentine@users.noreply.github.com>
2022-01-24 13:17:38 -0500

    This is my info

---
+++ myInfo.py
@@ -0,0 +1,7 @@
+#CS175L-01
+#Brandon Valentine
+#myInfo
+print("Hello my name is Brandon Valentine")
+print("My address is 602 Stamford Drive,Neptune,07753")
+print("My phone number is 732-870-7714")
+print("I am a Computer Science Major")      
