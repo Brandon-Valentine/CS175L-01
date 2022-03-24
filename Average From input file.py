755010ccd301687e3f9d2f85f818cd643bcd8493
Brandon-Valentine <91150793+Brandon-Valentine@users.noreply.github.com>
2022-03-24 13:05:21 -0400

    Add files via upload

--- Average From input file.py
+++ Average From input file.py
@@ -1,15 +1,21 @@
 def main():
-    number=open('numbers.txt','r')
+    try:
+        number=open('umbers.txt','r')
+    except IOError:
+        print('An error has occured trying to read the file')
     total=0
     count=0
     for line in number:
-        amount=float(line)
-        total=amount+total
-        total=float(total)
-        count=count+1
-        print(f'I read in {count} number(s) Current number is:  {amount:.2f} Total is:  {total:.2f}')
+        try:
+            amount=float(line)
+            total=amount+total
+            count=count+1
+            print(f'I read in {count} number(s) Current number is:  {amount:.2f} Total is:  {total:.2f}')
+        except ValueError:
+            print('Non-numeric data found in the file. Skipping...')
         average=(total)/(count)
         average=float(average)
     number.close()
     print('Average: ',average)
+    
 main()
